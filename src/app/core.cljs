332ad6e94bb70dc5e60be6c98941d7e4448bd4f3
Yosevu Kilonzo <yosevuk@gmail.com>
2019-12-16 22:44:19 -0500

    feat: set up client routing with bidi and pushy

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,20 +1,74 @@
 (ns app.core
-  (:require [reagent.core :as r]
-            [app.resource :as resource]))
+  (:require [app.resource :as rc]
+            [bidi.bidi :as bidi]
+            [pushy.core :as pushy]
+            [reagent.core :as r]))
 
-(defn app
-  []
-  (let [posts (resource/get-posts "src/posts")]
-       ;; (js/alert posts)
-       [:div
-         (for [post posts]
-           [:div
-            [:div (:metadata post)]
-            [:div (:html post)]])]))
+;; State
 
+(def state (r/atom { :posts (rc/get-posts "src/posts") }))
 
-(defn ^:export start
-  []
-  (r/render
-   [app]
+;; Routes
+
+(def app-routes
+  ["/" {"" :index
+        ["" :post-id] :post
+        ["" :page-id] :page}])
+
+(defn current-page []
+  (:current-page @state))
+
+;; Views
+
+(defn index []
+  [:div
+   [:h2 "index"]
+   (for [post (:posts @state)]
+     [:div {:key (:slug (:metadata post))}
+      [:a {:href (bidi/path-for
+                  app-routes
+                  :post
+                  :post-id
+                  (first (:slug (:metadata post))))}
+       (first (:slug (:metadata post)))]])])
+
+;; (defn index []
+;;   [:div [:h2 "/"]
+;;    ;; [:div [:a {:href (bidi/path-for app-routes :about)} "/about"]]
+;;    [:div [:a {:href (bidi/path-for app-routes :post :post-id :post-1)} "/:post-id" ]]])
+
+;; (defn about []
+;;   [:div [:h2 "/about"]
+;;    [:div [:a {:href (bidi/path-for app-routes :index)} "/"]]])
+
+(defn post [post-id]
+  [:div
+   [:div
+    [:a {:href (bidi/path-for app-routes :index)} "index"]
+    [:h2 post-id]
+    [:article "content"]]])
+
+;; Routing
+
+(defn pages [path]
+  (case (:handler (:current-page @state))
+    :index [index]
+    ;; :about [about]
+    :post [post (:post-id (:route-params (:current-page @state)))]
+    [[:div "default page"]]))
+
+(defn set-page! [match]
+  (swap! state assoc :current-page match))
+
+(def history
+  (pushy/pushy set-page! (partial bidi/match-route app-routes)))
+
+(defn app []
+  [:div (pages current-page)])
+
+;; Start
+
+(defn ^:export start! []
+  (pushy/start! history)
+  (r/render [app]
    (.getElementById js/document "app")))
