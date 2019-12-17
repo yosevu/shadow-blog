33ecd1168a912c320afecc50a05d74f6bf5f5a28
Yosevu Kilonzo <yosevuk@gmail.com>
2019-12-17 14:31:15 -0500

    feat(wip): not-found page

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -13,7 +13,8 @@
 (def app-routes
   ["/" {"" :index
         ["" :post-id] :post
-        ["" :page-id] :page}])
+        ["" :page-id] :page
+        true :not-found}])
 
 (defn current-page []
   (:current-page @state))
@@ -46,14 +47,19 @@
     [:a {:href (bidi/path-for app-routes :index)} "index"]
     [:div "another page"]]])
 
+(defn not-found []
+  [:div
+   [:p "Not Found"]])
+
 ;; Routing
 
 (defn pages [path]
+  ;; (js/alert (:current-page @state))
   (case (:handler (:current-page @state))
     :index [index]
     :page [page :another-page]
     :post [post (:post-id (:route-params (:current-page @state)))]
-    [[:div "default page"]]))
+    [not-found]))
 
 (defn set-page! [match]
   (swap! state assoc :current-page match))
