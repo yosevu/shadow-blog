08c5339801274e6c341f9d6a908519c25b4eadae
Yosevu Kilonzo <yosevuk@gmail.com>
2019-12-27 13:25:12 -0600

    feat: add cssnano and postcss-import
    - Refactor postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,18 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+    isProduction && require('@fullhuman/postcss-purgecss')({
       content: [
         './src/app/core.cljs',
       ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+    isProduction && require('cssnano')({
+      preset: 'default',
     }),
   ],
 };
