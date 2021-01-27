c0cdca236f872766b5496c5ec737a20409819ff7
Christian Westrom <c.westrom@westrom.xyz>
2021-01-27 17:39:45 -0500

    changed syntax to add classes by keywords

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -4,6 +4,7 @@
             [clojure.string :refer [split]]
             [pushy.core :as pushy]
             [reagent.core :as r]
+            [reagent.dom :as dom]
             ["highlight.js" :as hljs]))
 
 ;; State
@@ -24,61 +25,68 @@
 ;; Views
 
 (defn tag-template [tag]
-  [:a.text-blue-600.text-sm.t.ml-3.border-b.border-transparent.hover:border-blue-600
-   {:key tag
-    :href (bidi/path-for
-                   app-routes
-                   :tag
-                   :tag-id
-                   tag)}
+  [:a {:class '[text-blue-600 text-sm tl ml-3 border-b border-transparent hover:border-blue-600]
+       :key   tag
+       :href  (bidi/path-for
+                app-routes
+                :tag
+                :tag-id
+                tag)}
    tag])
 
 (defn tags [post]
-  [:div.-mt-px
+  [:div {:class '[-mt-px]}
    (for [tag (split (first (:tags (:metadata post))) " ")]
      (tag-template tag))])
 
+;; {:class '[]}
+
 (defn index [posts]
-  [:main.mt-12
+  [:main {:class '[mt-12]}
    (for [post posts]
-     [:div.my-4.py-4 {:key (first (:id (:metadata (last post))))}
-      [:a.text-lg.font-medium.border-b.border-transparent.hover:border-gray-900
-       {:href (bidi/path-for
-               app-routes
-               :post
-               :post-id
-               (first (:id (:metadata (last post)))))}
+     [:div  {:class '[my-4 py-4]
+             :key   (first (:id (:metadata (last post))))}
+      [:a
+       {:class '[text-lg font-medium border-b border-transparent hover:border-gray-900]
+        :href  (bidi/path-for
+                 app-routes
+                 :post
+                 :post-id
+                 (first (:id (:metadata (last post)))))}
        (first (:title (:metadata (last post))))]
-      [:div.flex.items-center.mt-1
-       [:time.text-sm.tracking-wide.mt-px (first (:date (:metadata (last post))))]
+      [:div {:class '[flex items-center mt-1]}
+       [:time
+        {:class '[text-sm tracking-wide mt-px]}
+        (first (:date (:metadata (last post))))]
        (tags (last post))]
-      [:p.tracking-wide.mt-2 (first (:subtitle (:metadata (last post))))]])])
+      [:p
+       {:class '[tracking-wide mt-2]} (first (:subtitle (:metadata (last post))))]])])
 
 (defn not-found []
-  [:div.mt-12
+  [:div {:class '[mt-12]}
    [:p "Not Found"]])
 
 (defn- highlight-block [node]
-  (if (nil? (.querySelector (r/dom-node node) "pre code"))
+  (if (nil? (.querySelector (dom/dom-node node) "pre code"))
     nil
-    (doseq [block (array-seq (.querySelectorAll (r/dom-node node) "pre code"))]
+    (doseq [block (array-seq (.querySelectorAll (dom/dom-node node) "pre code"))]
       (.highlightBlock hljs block))))
 
 (defn post [post-id]
   (r/create-class
-   {:component-did-mount highlight-block
-    :reagent-render
-    (fn []
-      (if (nil? ((keyword post-id) (:posts @state)))
-        (not-found)
-        [:div.mt-12
-         [:h2.text-2xl.font-medium
-          (first (:title (:metadata ((keyword post-id) (:posts @state)))))]
-         [:div.flex.mt-2.items-center
-          [:time.text-sm.tracking-wide.mt-px (first (:date (:metadata ((keyword post-id) (:posts @state)))))]
-          (tags ((keyword post-id) (:posts @state)))]
-         [:article.markdown.mt-6
-          {:dangerouslySetInnerHTML {:__html (:html ((keyword post-id) (:posts @state)))}}]]))}))
+    {:component-did-mount highlight-block
+     :reagent-render
+     (fn []
+       (if (nil? ((keyword post-id) (:posts @state)))
+         (not-found)
+         [:div {:class '[mt-12]}
+          [:h2 {:class '[text-2xl font-medium]}
+           (first (:title (:metadata ((keyword post-id) (:posts @state)))))]
+          [:div {:class '[flex mt-2 items-center]}
+           [:time {:class '[text-sm tracking-wide mt-px]} (first (:date (:metadata ((keyword post-id) (:posts @state)))))]
+           (tags ((keyword post-id) (:posts @state)))]
+          [:article {:class                   '[markdown mt-6]
+                     :dangerouslySetInnerHTML {:__html (:html ((keyword post-id) (:posts @state)))}}]]))}))
 
 (defn filter-by-tag [posts tag-id]
   (filter #(re-find (re-pattern tag-id) (first (:tags (:metadata (val %))))) posts))
@@ -88,23 +96,23 @@
 (defn pages [path]
   (case (:handler (:current-page @state))
     :index [index (:posts @state)]
-    :post [post (:post-id (:route-params (:current-page @state)))]
-    :tag [index (filter-by-tag (:posts @state) (:tag-id (:route-params (:current-page @state))))]
+    :post  [post (:post-id (:route-params (:current-page @state)))]
+    :tag   [index (filter-by-tag (:posts @state) (:tag-id (:route-params (:current-page @state))))]
     [not-found]))
 
 (defn set-page! [match]
   (swap! state assoc :current-page match))
 
 (defn app []
-  [:div.container.mx-auto.max-w-2xl.m-4.p-4.mt-10.text-gray-900
+  [:div {:class '[container mx-auto max-w-2xl m-4 p-4 mt-10 text-gray-900]}
    [:header
-    [:h1.text-gray-900.text-xl.leading-snug.tracking-wide
-     [:a.border-b.border-transparent.hover:border-gray-900
-      {:href (bidi/path-for app-routes :index)
+    [:h1 {:class '[text-gray-900 text-xl leading-snug tracking-wide]}
+     [:a {:class      '[ border-b border-transparent hover:border-gray-900]
+          :href       (bidi/path-for app-routes :index)
           :aria-label "Yosev, strange loop"}
       "Yosevu.strange-loop"]]
-    [:code.text-xs.tracking-tight
-     {:aria-label "Thread thoughts, read, evaluate, print"}
+    [:code {:class      '[text-xs tracking-tight]
+            :aria-label "Thread thoughts, read, evaluate, print"}
      "(-> thoughts read eval print)"]]
    (pages current-page)])
 
@@ -115,5 +123,5 @@
 
 (defn ^:export start! []
   (pushy/start! history)
-  (r/render [app]
-            (.getElementById js/document "app")))
+  (dom/render [app]
+              (.getElementById js/document "app")))
